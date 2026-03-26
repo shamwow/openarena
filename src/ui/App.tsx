325486711f00ac44bed23b47bbf585bec33e505c
Shahmeer Navid <shahmeernavid@gmail.com>
2026-03-26 03:58:52 -0400

    orchestrate: implementation changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './arena-theme.css';
 import { GameBoard } from './components/GameBoard';
+import { Agentation } from 'agentation';
 
 const App: React.FC = () => {
-  return <GameBoard />;
+  return (
+    <>
+      <GameBoard />
+      {import.meta.env.DEV && <Agentation />}
+    </>
+  );
 };
 
 export default App;
