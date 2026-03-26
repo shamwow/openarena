e66636c0b85bdfa7968893514b255af3ddf3e0f0
Shahmeer Navid <shahmeernavid@gmail.com>
2026-03-26 04:10:46 -0400

    orchestrate: address code review cycle 4

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './arena-theme.css';
 import { GameBoard } from './components/GameBoard';
-import { Agentation } from 'agentation';
+
+const LazyAgentation = React.lazy(() =>
+  import('agentation').then((m) => ({ default: m.Agentation }))
+);
 
 const App: React.FC = () => {
   return (
     <>
       <GameBoard />
-      {import.meta.env.DEV && <Agentation />}
+      {import.meta.env.DEV && (
+        <Suspense>
+          <LazyAgentation />
+        </Suspense>
+      )}
     </>
   );
 };
